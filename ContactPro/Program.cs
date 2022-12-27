10a306ecefca443c7ae6106d81221bdbec811df0
brianley99 <Brianley99@gmail.com>
2022-12-27 04:10:51 -0800

    fix syntax error

--- ContactPro/Program.cs
+++ ContactPro/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddScoped<IImageService, ImageService>();
 builder.Services.AddScoped<IAddressBookService, AddressBookService>();
 builder.Services.AddScoped<IEmailSender, EmailService>();
 
-builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings")); ;
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
 
 
 var app = builder.Build();
